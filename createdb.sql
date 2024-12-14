bc1a9f1c1712f5186fa77473c70936a7f2592d70
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-12-14 23:16:39 +0300

    создание других таблиц

--- createdb.sql
+++ createdb.sql
@@ -34,7 +34,9 @@ CREATE TABLE book
 (
   book_id INTEGER PRIMARY KEY, -- уникальный ключ в таблице
   title TEXT NOT NULL, -- NOT NULL нельзя передавать пустое значение
-  isbn VARCHAR(32) NOT NULL
+  isbn VARCHAR(32) NOT NULL,
+  -- пример создания внешнего ключа
+  fk_publisher_id INTEGER  REFERENCES publisher(publisher_id) NOT NULL
 )
 
 --удаление таблиц
@@ -45,13 +47,13 @@ DROP TABLE book
 
 INSERT INTO book
 VALUES
-(1, 'новое описние книги', '33321231'),
-(2, 'второе описание книги', '43928439'),
-(3, 'супер классная книга', '77438832'),
-(4, 'отличная книга', '33321231'),
-(5, 'просто новая книга', '7878393'),
-(6, 'эта книга так себе', '894830209'),
-(7, 'просто фигня', '4389848899');
+(1, 'новое описние книги', '33321231', 2),
+(2, 'второе описание книги', '43928439', 3),
+(3, 'супер классная книга', '77438832', 3),
+(4, 'отличная книга', '33321231', 3),
+(5, 'просто новая книга', '7878393', 1),
+(6, 'эта книга так себе', '894830209', 4),
+(7, 'просто фигня', '4389848899', 4);
 
 
 INSERT INTO publisher
@@ -64,3 +66,42 @@ VALUES
 -- проверка вставки данных
 SELECT *
 from book
+
+
+-- Добавление колл=онки в таблицу
+ALTER TABLE book
+ADD COLUMN fk_publisher_id INTEGER;
+
+ALTER TABLE book
+ADD CONSTRAINT fk_book_publisher_id FOREIGN KEY(fk_publisher_id) REFERENCES publisher(publisher_id);
+
+CREATE TABLE person
+(
+  person_id INTEGER PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT not NULL
+);
+
+CREATE TABLE passport
+(
+  passport_id INTEGER PRIMARY KEY,
+  serias VARCHAR(10) NOT NULL,
+  regirstration TEXT NOT NULL,
+  fk_person_id INTEGER REFERENCES person(person_id) NOT NULL
+);
+
+INSERT INTO person 
+VALUES
+(1, 'Павел', 'Егоров'),
+(2, 'Иван', 'Иввнов'),
+(3, 'Пётр', 'Петров'),
+(4, 'Степан', 'Степанов'),
+(5, 'Павел', 'Рожков');
+
+INSERT INTO passport
+VALUES
+(1, '1212121212', 'MSK', 2),
+(2, '1313131313', 'CHB', 3),
+(3, '1414141414', 'MSK', 4),
+(4, '1111111111', 'SPB', 1),
+(1, '1212121212', 'MSK', 5),
