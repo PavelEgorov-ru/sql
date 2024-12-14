636aef3870d1bca5e3b773cf7e75af9c44074db3
PavelEgorov-ru <pkotofffff@yandex.ru>
2024-12-14 21:59:51 +0300

    вставка и удаление данных

--- createdb.sql
+++ createdb.sql
@@ -1,4 +1,4 @@
-# пример ссоздания БД
+-- пример ссоздания БД
 
 CREATE DATABASE testdb
     WITH
@@ -9,7 +9,7 @@ CREATE DATABASE testdb
     IS_TEMPLATE = False;
 
 
-# Удаление БД
+-- Удаление БД
 
 SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
@@ -17,3 +17,50 @@ WHERE pg_stat_activity.datname = 'testdb'
 AND pid <> pg_backend_pid()
 
 DROP DATABASE testdb
+
+
+-- Создание таблицы
+
+CREATE TABLE publisher
+(
+  publisher_id INTEGER PRIMARY KEY, -- уникальный ключ в таблице
+  org_name VARCHAR(128) NOT NULL, -- NOT NULL нельзя передавать пустое значение
+  addres TEXT NOT NULL,
+  -- CONSTRAINT pk_publisher_id PRIMARY KEY (publisher_id)
+);
+
+
+CREATE TABLE book
+(
+  book_id INTEGER PRIMARY KEY, -- уникальный ключ в таблице
+  title TEXT NOT NULL, -- NOT NULL нельзя передавать пустое значение
+  isbn VARCHAR(32) NOT NULL
+)
+
+--удаление таблиц
+DROP TABLE publisher;
+DROP TABLE book
+
+-- всиавить данные в таблицу
+
+INSERT INTO book
+VALUES
+(1, 'новое описние книги', '33321231'),
+(2, 'второе описание книги', '43928439'),
+(3, 'супер классная книга', '77438832'),
+(4, 'отличная книга', '33321231'),
+(5, 'просто новая книга', '7878393'),
+(6, 'эта книга так себе', '894830209'),
+(7, 'просто фигня', '4389848899');
+
+
+INSERT INTO publisher
+VALUES
+(1, 'Питер', 'SPB'),
+(2, 'Феникс', 'MSK'),
+(3, 'Новая книга', 'KZN'),
+(4, 'Волна', 'EKB')
+
+-- проверка вставки данных
+SELECT *
+from book
